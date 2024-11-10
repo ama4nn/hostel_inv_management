067b03010da813a39db67e6501796299f136f8eb
Abel Varghese <abelshinevarghese@gmail.com>
2024-11-11 02:49:11 +0530

    Updated SQL file

--- nodelogin.sql
+++ nodelogin.sql
@@ -1,38 +1,543 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2024 at 11:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE Room (
-    room_no VARCHAR(4) PRIMARY KEY,
-    capacity INT NOT NULL,
-    block_id CHAR NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Table for Resource
-CREATE TABLE Resource (
-    resource_id INT PRIMARY KEY AUTO_INCREMENT,
-    resource_name VARCHAR(100) NOT NULL,
-    quantity INT,
-    price INT,
-    p_date DATE
-);
+--
+-- Database: `nodelogin`
+--
 
--- Table for Resource Requests
-CREATE TABLE ResourceRequests (
-    request_id INT PRIMARY KEY AUTO_INCREMENT,
-    room_no VARCHAR(4),
-    resource_id INT,
-    student_id VARCHAR(8) NOT NULL,
-    quantity INT NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    request_date DATE,
-    INDEX idx_student_id (student_id)  -- Adding an index on student_id
-) ENGINE=InnoDB;
+-- --------------------------------------------------------
 
--- Table for Maintenance Log
-CREATE TABLE MaintenanceLog (
-    log_id INT PRIMARY KEY AUTO_INCREMENT,
-    date_completed DATE,
-    request_id INT,
-    student_id VARCHAR(8),
-    FOREIGN KEY (request_id) REFERENCES ResourceRequests(request_id),
-    FOREIGN KEY (student_id) REFERENCES ResourceRequests(student_id)
-) ENGINE=InnoDB;
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(10) NOT NULL,
+  `patient_name` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `doctor_name` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `patient_name`, `department`, `doctor_name`, `date`, `time`, `email`, `phone`) VALUES
+(6, 'Test', 'Orthopedics', 'B.rabbit', '26/03/2020', '10:43 AM', 'gmhs13@yopmail.com', '7865641399'),
+(10, 'Test123', 'Orthopedics', 'doc test', '18/03/2020', '1:41 AM', 'gmhs13@yopmail.com', '7865641399'),
+(15, 'ads', 'Dentists', 'asdf', '10/11/2024', '1:54 AM', 'fasjd@smklnfad', 'asdf'),
+(16, 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined', 'undefined');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complain`
+--
+
+CREATE TABLE `complain` (
+  `id` int(10) NOT NULL,
+  `message` varchar(500) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `subject` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `complain`
+--
+
+INSERT INTO `complain` (`id`, `message`, `name`, `email`, `subject`) VALUES
+(1, 'wdawdws', 'awdawd', 'awdw', 'adwwd'),
+(2, 'Definition of complain. intransitive verb. 1 : to express grief, pain, or discontent complaining about the weather. 2 : to make a formal accusation or charge He threatened to complain of him to the captain.', 'Isaiah L. Smith', 'gmhs13@yopmail.com', 'dafsgd'),
+(3, 'redtfyguhijo', 'simanto', 'gmhs13@yopmail.com', 'ytguijopk['),
+(4, 'abcabcbacbacbabc', 'Isaiah L. Smith', 'gmhs13@yopmail.com', 'dadsvfbgfng');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL,
+  `department_name` varchar(255) NOT NULL,
+  `department_desc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `department_name`, `department_desc`) VALUES
+(11, 'Intensive Care Unit (ICU)', 'What is an intensive care unit (ICU)? Intensive care refers to the specialised treatment given to patients who are acutely unwell and require critical medical care. An intensive care unit (ICU) provides the critical care and life support for acutely ill a'),
+(16, 'Neurology', 'Neurology is a branch of medicine dealing with disorders of the nervous system. Neurology deals with the diagnosis and treatment of all categories of conditions and disease involving the central and peripheral nervous systems, including their coverings'),
+(17, 'Opthalmology', 'dfvgbhjnkml'),
+(18, 'Orthopedics', 'dfyuyuo'),
+(19, 'Cancer Department', 'asyckuauhcioa'),
+(20, 'ENT department', 'savcjaub');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `id` int(10) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dob` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `image` text NOT NULL,
+  `department` varchar(50) NOT NULL,
+  `biography` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`id`, `first_name`, `last_name`, `email`, `dob`, `gender`, `address`, `phone`, `image`, `department`, `biography`) VALUES
+(39, 'AV', 'Stranger', 'gmhs13@yopmail.com', '18/03/2020', 'male', '3125  Elkview Drive, Miami,33169', '7865641399', 'user-02.jpg', 'Intensive Care Unit (ICU)', 'dawfesgrthy'),
+(41, 'SHAHID AFRIDI', 'ZIHAD', 'gmhs13@yopmail.com', '18/03/2020', 'male', '3125  Elkview Drive, Miami,33169', '7865641399', 'reservation.png', 'Intensive Care Unit (ICU)', 'awdsaef');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `join_date` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `salary` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `name`, `email`, `contact`, `join_date`, `role`, `salary`) VALUES
+(18, 'user2', 'gmhs13@yopmail.com', '1568496', '26/03/2020', 'Receptionist', '3000'),
+(19, 'abc', 'gmhs13@yopmail.com', '7865641399', '26/03/2020', 'Pharmacist', '3000'),
+(20, 'Abc', 'gmhs13@yopmail.com', '0159653', '26/03/2020', 'Pharmacist', '651320');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaves`
+--
+
+CREATE TABLE `leaves` (
+  `id` int(10) NOT NULL,
+  `employee` varchar(255) NOT NULL,
+  `emp_id` int(10) NOT NULL,
+  `leave_type` varchar(255) NOT NULL,
+  `date_from` varchar(255) NOT NULL,
+  `date_to` varchar(255) NOT NULL,
+  `reason` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leaves`
+--
+
+INSERT INTO `leaves` (`id`, `employee`, `emp_id`, `leave_type`, `date_from`, `date_to`, `reason`) VALUES
+(7, 'Isaiah L. Smith', 4, 'Medical Leave', '26/03/2020', '31/03/2020', 'acdsvfbgnh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `email`) VALUES
+(1, 'test', 'test', 'gmhs13@yopmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenancelog`
+--
+
+CREATE TABLE `maintenancelog` (
+  `log_id` int(11) NOT NULL,
+  `date_completed` date DEFAULT NULL,
+  `request_id` int(11) DEFAULT NULL,
+  `student_id` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resource`
+--
+
+CREATE TABLE `resource` (
+  `resource_id` int(11) NOT NULL,
+  `resource_name` varchar(100) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `p_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resourcerequests`
+--
+
+CREATE TABLE `resourcerequests` (
+  `request_id` int(11) NOT NULL,
+  `room_no` varchar(4) DEFAULT NULL,
+  `resource_id` int(11) DEFAULT NULL,
+  `student_id` varchar(8) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `request_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `resourcerequests`
+--
+
+INSERT INTO `resourcerequests` (`request_id`, `room_no`, `resource_id`, `student_id`, `quantity`, `status`, `request_date`) VALUES
+(7, 'B203', 1, '23BCS295', 6, 'Approved', '0000-00-00'),
+(8, 'A405', 6, '23BCS294', 6, 'Pending', '2024-02-11'),
+(9, 'B201', 2, '23BCS284', 2, 'Pending', '2024-11-10'),
+(10, 'A402', 1, '23BCS294', 12345, 'Pending', '2024-11-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `room_no` varchar(4) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `block_id` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+CREATE TABLE `store` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `p_date` varchar(255) NOT NULL,
+  `expire` varchar(255) NOT NULL,
+  `expire_end` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `store`
+--
+
+INSERT INTO `store` (`id`, `name`, `p_date`, `expire`, `expire_end`, `price`, `quantity`) VALUES
+(4, 'Napa', '20/03/2020', '2', '19/03/2020', '8', '100'),
+(8, 'Seclo', '24/03/2020', '2', '24/03/2020', '5', '200'),
+(9, 'Napa', '24/03/2020', '1 ', '19/03/2020', '10', '20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp`
+--
+
+CREATE TABLE `temp` (
+  `id` int(10) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `temp`
+--
+
+INSERT INTO `temp` (`id`, `email`, `token`) VALUES
+(19, 'test555@yopmail.com', '46fn0pl3'),
+(19, 'test555@yopmail.com', 'w6pvf2oq'),
+(21, 'te555@yopmail.com', '9sfs6gu8'),
+(19, 'test555@yopmail.com', '46fn0pl3'),
+(19, 'test555@yopmail.com', 'w6pvf2oq'),
+(21, 'te555@yopmail.com', '9sfs6gu8');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `email_status`) VALUES
+(9, 'zihad', 'zihad.1d@yopmail.com', '123', 'verified'),
+(19, 'test', 'test555@yopmail.com', '123', 'verified'),
+(20, 'abc', 'gmhs13@yopmail.com', '12345', 'not_verified'),
+(21, 'alamin', 'te555@yopmail.com', 'abc', 'verified');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verify`
+--
+
+CREATE TABLE `verify` (
+  `id` int(10) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `verify`
+--
+
+INSERT INTO `verify` (`id`, `username`, `email`, `token`) VALUES
+(4, 'zihad', 'zihad.1d@yopmail.com', 'lp5ux5ik'),
+(14, 'test', 'test555@yopmail.com', '3udlo9v6'),
+(15, 'abc', 'gmhs13@yopmail.com', 'w2px024k'),
+(16, 'alamin', 'te555@yopmail.com', 'ix8enxdh');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `complain`
+--
+ALTER TABLE `complain`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `leaves`
+--
+ALTER TABLE `leaves`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `maintenancelog`
+--
+ALTER TABLE `maintenancelog`
+  ADD PRIMARY KEY (`log_id`),
+  ADD KEY `request_id` (`request_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `resource`
+--
+ALTER TABLE `resource`
+  ADD PRIMARY KEY (`resource_id`);
+
+--
+-- Indexes for table `resourcerequests`
+--
+ALTER TABLE `resourcerequests`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `idx_student_id` (`student_id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`room_no`);
+
+--
+-- Indexes for table `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `verify`
+--
+ALTER TABLE `verify`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `complain`
+--
+ALTER TABLE `complain`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `leaves`
+--
+ALTER TABLE `leaves`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `maintenancelog`
+--
+ALTER TABLE `maintenancelog`
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `resource`
+--
+ALTER TABLE `resource`
+  MODIFY `resource_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `resourcerequests`
+--
+ALTER TABLE `resourcerequests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `store`
+--
+ALTER TABLE `store`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `verify`
+--
+ALTER TABLE `verify`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `maintenancelog`
+--
+ALTER TABLE `maintenancelog`
+  ADD CONSTRAINT `maintenancelog_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `resourcerequests` (`request_id`),
+  ADD CONSTRAINT `maintenancelog_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `resourcerequests` (`student_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
