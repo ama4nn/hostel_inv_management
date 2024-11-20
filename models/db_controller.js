34856b0703f93b7ad6d72841c8ba79d731fa9477
ama4nn <pix3l20114@gmail.com>
2024-11-20 14:26:39 +0400

    Segmentation of db_controller.js Implementation of dotenv for security.

--- models/db_controller.js
+++ models/db_controller.js
@@ -1,13 +1,14 @@
 var mysql = require("mysql");
+require("dotenv").config()
 
 //module.exports = router;
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodelogin",
-  port: 3306
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
 
 con.connect((err) => {
